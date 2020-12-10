e7e440dd7bf19f2156526a65cbd58a38457c25cd
Rodrigo0Sanchez <sanchez.salinas.gianpier.rodrigo@iisgalvanimi.edu.it>
2020-12-10 10:47:14 +0000

    1 non funzionante

---
+++ Cibo/app/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { FoodfactsService } from './foodfacts.service';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  query: string;
+  title = 'Cibo';
+  obsTrack: Observable<Object>;
+  results: any;
+  // faccio iniettare lo spotify service e faccio una ricerca
+  constructor(public foodfact: FoodfactsService) {
+
+  }
+
+  submit(query:HTMLInputElement): void {
+    if (!query.value) {
+      return;
+    }
+    this.query = query.value;
+    this.obsTrack = this.foodfact.searchFood(this.query);
+    this.obsTrack.subscribe((data) => this.results = data);
+  }
+}
---
+++ Cibo/app/src/app/foodfacts.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+//Dichiaro che il servizio è iniettabile agli altri componenti a partire dal componente root
+@Injectable({
+  providedIn: 'root'
+})
+export class FoodfactsService {
+   //url per oauth: https://developer.spotify.com/console/get-search-item/
+  //Ottengo il modulo HttpClient
+  constructor(private http: HttpClient) { }
+
+  searchFood(query: string) {
+    const url = `https://world.openfoodfacts.org/cgi/search.pl?search_terms=${query}&page_size=2&json=true`;
+    const headers = new HttpHeaders({
+
+    });
+
+    let obsTracks = this.http.get(url, { headers });
+    return obsTracks;
+ //Ritorno un observable ai componenti che richiedono il servizio
+  }
+}
